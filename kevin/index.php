b2fc6c2a65c9674843162eaa64e3043e45115f3a
jackson29 <jacksonssantos@bol.com.br>
2018-09-14 15:19:04 -0300

    Adicionei index.php

---
+++ kevin/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Equipes </title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.css">
+</head>
+<style>
+	body {
+		padding-top: 25px;
+	}
+</style>
+<body>
+	<div class="ui container">
+
+		<h1> Seu Ponto Turístico </h1>
+			
+		<p> Nosso projeto será um mapeamento de pontos turísticos </p>
+		<h2> Integrantes da Equipe </h2>
+		
+	<div>
+		<table class="ui celled table">
+			<tr>
+				<th> Nome </th>
+				<th> E-mail </th>
+				<th> Contato </th>
+			</tr>
+			<tr>
+				<td> Luigi Martins </td>
+				<td> luigi.martins.355@gmail.com </td>
+				<td> (81) 98641-9225 </td>
+			</tr>
+			<tr>
+				<td> Kevin Sousa </td>
+				<td> kevinsmoura@hotmail.com </td>
+				<td> (81) 99728-1758 </td>
+			</tr>
+			<tr>
+				<td> Rhodolfo Santana </td>
+				<td> negrodrama.rs95@gmail.com </td>
+				<td> (81) 98798-1203 </td>
+			</tr>
+			<tr>
+				<td> Erickson Ferreira </td>
+				<td> erickson.rinho@hotmail.com </td>
+				<td> (81) 98625-1208 </td>
+			</tr>
+			<tr>
+				<td> Cleyton Vinicius </td>
+				<td> cleytonv104@gmail.com </td>
+				<td> (81) 97303-0241 </td>
+			</tr>
+			<tr>
+				<td> Jhonny de Farias </td>
+				<td> jhonnyfarias87@gmail.com </td>
+				<td> (81) 99372-4644 </td>
+			</tr>
+			<tr>
+				<td> Naadabe Farias </td>
+				<td> naadabefarias@gmail.com </td>
+				<td> (81) 99431-8653 </td>
+			</tr>
+		</table>
+		</div>
+	</div>
+</body>
+</html>
