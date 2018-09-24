5b082b34f55b690282eae71b5e1dc214dc45d30c
Kevin <kevinsmoura@hotmail.com>
2018-09-24 20:38:02 -0300

    Mesclar as duas Tabelas

---
+++ mysql/banco.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Users (
+    id int auto_increment,
+    name varchar(255) NOT NULL,
+    user varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    role varchar(255) NOT NULL,
+    primary key(id)
+);
+
+CREATE TABLE pontos_turisticos(
+
+	id int not null auto_increment,
+	nome_ponto varchar(200) not null,
+	logradouro varchar(200) not null,
+	bairro varchar(200) not null,
+	numero_ponto varchar(50),
+		constraint pkpontos primary key (id)
+        
+);
