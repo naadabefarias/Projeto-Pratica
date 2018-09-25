698bdbbfd6207316b8d6fbd0c1d14331f711d33b
Erickson Ferreira <erickson.rinho@hotmail.com>
2018-09-24 22:24:54 -0300

    Criando a View Login

---
+++ View/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bem Vindos ao Login</title>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="Login_Style.css">
+</head>
+<body>
+
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Guias de Titulos -->
+
+    <!-- Icone -->
+    <div class="fadeIn first">
+      <img src="SantaCruz.jpg" id="icon" alt="User Icon" />
+    </div>
+
+    <!-- Login do Formulario -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuario">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Senha">
+      <input type="submit" class="fadeIn fourth" value="Entrar">
+    </form>
+
+    <!-- Lembrar Senha -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Esqueceu a senha?</a>
+    </div>
+
+  </div>
+</div>
+	
+</body>
+</html>
