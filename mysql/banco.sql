29de0ab9e324134439c3e92d31b4978f6a143ef8
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2018-09-24 22:18:27 -0300

    finalizando e acrescentando a parte de Cleyton que foi cadastro e arquivo de insercao no banco

--- mysql/banco.sql
+++ mysql/banco.sql
@@ -3,8 +3,6 @@ CREATE TABLE Users (
     name varchar(255) NOT NULL,
     user varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
-    role varchar(255) NOT NULL,
-    primary key(id)
 );
 
 CREATE TABLE pontos_turisticos(
--- mysql/conexao.php
+++ mysql/conexao.php
@@ -1,6 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$username = "root";
+	$password = "";
+	$conn = new PDO('mysql:host=localhost;port=3306;dbname=', $username, $password);
+	?>
 
-<?php
-$username = "root";
-$password = "";
-$conn = new PDO('mysql:host=localhost;port=3306;dbname=task', $username, $password);
-?>
+	<?php
+	$nome = $_POST['nome'];
+	$user = $_POST['user'];
+	$pass = $_POST['pass'];
+
+	$sql = "INSERT INTO Users(name, user, password)
+		VALUES('$nome', '$user', '$pass')";
+	$query = $conn->prepare($sql);
+	var_dump($query);
+	$query->bindParam(':nome', $nome);
+	$query->bindParam(':user', $user);
+	$query->bindParam(':pass', $pass);
+	$stmt = $query->execute();
+
+	header('index.php');
+	?>
+</body>
+</html>
