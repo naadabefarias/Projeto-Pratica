39fec47a08519c2fbee51a8e775d15f81f23898d
naadabefarias <naadabefarias@gmail.com>
2018-09-26 18:36:51 -0300

    adc session

--- Projeto-Pratica-master/Controller/conexao.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$username = "root";
-	$password = "luigi355";
-	$conn = new PDO('mysql:host=localhost;port=3306;dbname=banco', $username, $password);	
-?>
--- View/cadastrar_pontos.php
+++ View/cadastrar_pontos.php
@@ -1,3 +1,12 @@
+<?php
+
+session_start();
+if (!isset($_SESSION)['user']) {
+	header('location: login.php');
+	exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,3 +40,4 @@
 	</div>
 </body>
 </html>
+
