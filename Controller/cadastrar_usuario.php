ad20fe6ed38bd2fe6c350b5eb7cfa9f995d5631e
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2018-09-27 14:48:32 -0300

    Tentativa de evitar duplicações no cadastro de usuarios

--- Controller/cadastrar_usuario.php
+++ Controller/cadastrar_usuario.php
@@ -3,20 +3,35 @@
 
 	if(isset($_POST['nome']) && isset($_POST['user']) && isset($_POST['pass'])){
 
+
 		$nome = $_POST['nome'];
 		$user = $_POST['user'];
 		$pass = $_POST['pass'];
 
-		$sql = "INSERT INTO Users(name, user, password)
-			VALUES(:nome, :user, :pass)";
-		$query = $conn->prepare($sql);
-		var_dump($query);
-		$query->bindParam(':nome', $nome);
-		$query->bindParam(':user', $user);
-		$query->bindParam(':pass', $pass);
-		$stmt = $query->execute();
+		$checking = "SELECT * FROM Users WHERE user = '".$user."'";
+		$queryOne = $conn->prepare($checking);
+	 	$stmt = $queryOne->execute();
+			var_dump($stmt);
+		if ($stmt->rowCount() > 1) {
+			"<script> 
+				alert('Este usuário já exite');
+			</script>"; 
+			exit();
+		}
 
-		header('Location: ../View/login.php');
+		else {
+			
+			$sql = "INSERT INTO Users(name, user, password)
+				//VALUES(:nome, :user, :pass)";
+			$query = $conn->prepare($sql);
+			$query->bindParam(':nome', $nome);
+			$query->bindParam(':user', $user);
+			$query->bindParam(':pass', $pass);
+			$stmt = $query->execute();
+
+			header('Location: login.php');
+		}
+		 
 		
 	}
 
