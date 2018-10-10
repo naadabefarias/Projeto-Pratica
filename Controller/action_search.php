0149355fc65b05c1c5e3935b7a690ee3c95d4d74
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2018-10-10 14:58:36 -0300

    reajustes input text

--- Controller/action_search.php
+++ Controller/action_search.php
@@ -7,26 +7,30 @@ if (isset($_POST["query"])){
 	$output = '';
 
 
-$checking=("SELECT * FROM pontos_turisticos WHERE nome_ponto LIKE :pesquisa");
+	$checking=("SELECT * FROM pontos_turisticos WHERE nome_ponto LIKE :pesquisa");
 
-		$queryOne = $conn->prepare($checking);
-		$queryOne->bindValue(':pesquisa', '%'.$dado.'%');
-		$return = $queryOne -> execute();
-if ($return >= 1 ) {
+	$queryOne = $conn->prepare($checking);
+	$queryOne->bindValue(':pesquisa', '%'.$dado.'%');
+	$return = $queryOne -> execute();
+	
+	
 		$stmt = $queryOne->fetchAll();
 		$output = '<ul class="listas_result">';
+		if ($stmt!=null){
+		
 		
 		for ($i = 0; $i < sizeof($stmt); $i++){
 			$output .= '<li>' .$stmt[$i]['nome_ponto']. '</li>';
 		
 		}
 	}
-	else { 
-$output .= '<li> nada encontrado</li>';
+		else { 
+			$output .= '<li> nada encontrado</li>';
 	
 	}
-$output .= '</ul>';
-echo $output;
+	$output .= '</ul>';
+	echo $output;
+
 }
 
 ?>
