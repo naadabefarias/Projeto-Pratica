2f1b1db7c582af126c3978300ad1a168609e3258
CleytonVieira <cleytonv104@gmail.com>
2018-10-10 15:19:39 -0300

    novas correções de erros

--- Controller/action_search.php
+++ Controller/action_search.php
@@ -7,10 +7,9 @@ if (isset($_POST["query"])){
 	$output = '';
 
 
-<<<<<<< HEAD
+
 	$checking=("SELECT * FROM pontos_turisticos WHERE nome_ponto LIKE :pesquisa");
-=======
-<<<<<<< HEAD
+
 	$checking=("SELECT * FROM pontos_turisticos WHERE nome_ponto LIKE :pesquisa");
 
 	$queryOne = $conn->prepare($checking);
@@ -34,42 +33,6 @@ if (isset($_POST["query"])){
 	}
 	$output .= '</ul>';
 	echo $output;
-=======
-$checking=("SELECT * FROM pontos_turisticos WHERE nome_ponto LIKE :pesquisa");
->>>>>>> 9291fb40a3eda9cff424d59e217e91596e9513f6
-
-	$queryOne = $conn->prepare($checking);
-	$queryOne->bindValue(':pesquisa', '%'.$dado.'%');
-	$return = $queryOne -> execute();
-	
-	
-		$stmt = $queryOne->fetchAll();
-		$output = '<ul class="listas_result">';
-		if ($stmt!=null){
-		
-		
-		for ($i = 0; $i < sizeof($stmt); $i++){
-			$output .= '<li>' .$stmt[$i]['nome_ponto']. '</li>';
-		
-		}
-	}
-<<<<<<< HEAD
-		else { 
-			$output .= '<li> nada encontrado</li>';
-	
-	}
-	$output .= '</ul>';
-	echo $output;
-
-=======
-	else { 
-
-	
-	}
-$output .= '</ul>';
-echo $output;
->>>>>>> f326df1f652ee40d77145a326ef838611901bfd4
->>>>>>> 9291fb40a3eda9cff424d59e217e91596e9513f6
 }
 
 ?>
