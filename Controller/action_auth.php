043deeaeecafea4e7c5bbc78801e2639e4dc22e3
Luigi Martins <luigi.martins.355@gmail.com>
2018-10-23 23:12:06 -0300

    adcionando relacionamentos nas tabelas, lembrem de mudar o banco

--- Controller/action_auth.php
+++ Controller/action_auth.php
@@ -6,14 +6,16 @@
 	$senha = $_POST['senha'];
 	$private = sha1($senha);
 
-	$consulta=$conn->prepare("SELECT user AND password FROM Users WHERE user=? AND password=? ");
+	$consulta=$conn->prepare("SELECT id, user,password FROM Users WHERE user=? AND password=? ");
 	$consulta->bindParam(1,$login);
 	$consulta->bindParam(2,$private);
 	$consulta->execute();
 	
 	if ($consulta->rowCount() >= 1){
+		$dados = $consulta->fetch();
 		session_start();
-		$_SESSION['user'] = $login;
+		$_SESSION['id']  = $dados['id'];
+		$_SESSION['user'] = $login; 
 		header("Location: /index.php");
 
 	} else {
