a34196d960ac535f1e7812eda2cd3f4df5684d6c
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2018-10-24 00:03:45 -0300

    vei tinha 2 arquivos rodape e footer vamos fazer uma limpeza ae flw vlw

--- rodape.php
+++ rodape.php
@@ -2,8 +2,13 @@
 	<div id="foot"></div>
 		<footer class="page-footer font-small blue">
 		  	<!-- Copyright -->
-
-			<div class="footer-copyright text-center py-3">© 2018 Copyright:<strong>IFPE</strong></div>
+			
+			
+			<div class="footer-copyright text-center py-3">
+			©2018 Copyright:<strong>IFPE</strong>
+			<ul>
+				</ul>
+			</div>
 		</footer>
 	</div>
 </body>
