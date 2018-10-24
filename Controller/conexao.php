e03d98d084f9e78c683a2843de59ecbdc4e1a84f
Kevin <kevinsmoura@hotmail.com>
2018-10-24 19:08:01 -0300

    Alterar acript do banco e footer

--- Controller/conexao.php
+++ Controller/conexao.php
@@ -6,6 +6,6 @@
 	// $conn  =  new PDO('mysql:host=localhost;porta=3306;dbname=id7135713_ifpe', $username , $password);
 	
 	$username = "root";
-	$password = "luigi355";
-	$conn = new PDO('mysql:host=localhost;porta=3306;dbname=banco',$username,$password);
+	$password = "kevin";
+	$conn = new PDO('mysql:host=localhost;porta=3306;dbname=ponto',$username,$password);
 ?>
--- mysql/banco.sql
+++ mysql/banco.sql
@@ -1,3 +1,16 @@
+drop database if exists ponto;
+create database if not exists ponto;
+
+use ponto;
+
+CREATE TABLE `Users` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(255) NOT NULL,
+ `user` varchar(255) NOT NULL,
+ `password` varchar(255) NOT NULL,
+ PRIMARY KEY (`id`)
+);
+
 CREATE TABLE `pontos_turisticos` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `user_id` int(11) NOT NULL,
@@ -9,11 +22,4 @@ CREATE TABLE `pontos_turisticos` (
  PRIMARY KEY (`id`),
  KEY `user_id` (`user_id`),
  CONSTRAINT `pk_users` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) 
-CREATE TABLE `Users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `name` varchar(255) NOT NULL,
- `user` varchar(255) NOT NULL,
- `password` varchar(255) NOT NULL,
- PRIMARY KEY (`id`)
-) 
+); 
