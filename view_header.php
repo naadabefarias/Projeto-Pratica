ac11c1c54bf8f25443f8dfcfebde093e5a209e96
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2018-10-29 14:01:06 -0300

    removendo pesquisar do footer

--- view_header.php
+++ view_header.php
@@ -19,7 +19,6 @@ session_start();
 	<!-- Barra superior -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap core CSS -->
-  
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     
