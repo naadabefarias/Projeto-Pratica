3a653ef522de021bb0477600c721d11a476fb6f9
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2018-10-29 10:54:45 -0300

    ALterando rodapé

--- view_header.php
+++ view_header.php
@@ -17,6 +17,12 @@ session_start();
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<link rel="icon" type="image/png" href="images/icon.png">
 	<!-- Barra superior -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
 </head>
 <body>
 
