7a4b7062cc6ae61cf7d08f7e2949b7f519de1579
Erickson Ferreira <erickson.rinho@hotmail.com>
2018-11-09 18:53:47 -0300

    Corrigindo as Avaliaçacoes

--- Controller/action_avaliar.php
+++ Controller/action_avaliar.php
@@ -5,36 +5,15 @@ include "conexao.php";
 date_default_timezone_set("America/Sao_Paulo");
 setlocale(LC_ALL, 'pt_BR');
 
-$id   		= $_SESSION['id'];
-$user 		= $_SESSION['user'];
 $idPonto 	= $_GET['id'];
 $estrela 	= $_POST['estrela'];
 $created = date_create();
 $date = date_format($created, 'd-m-y');
 
-	$consulta=$conn->prepare("SELECT user_id,ponto_id FROM avaliacoes WHERE user_id=? AND ponto_id=? ");
-	$consulta->bindParam(1,$id);
-	$consulta->bindParam(2,$idPonto);
-	$consulta->execute();
-
-	
-	if ($consulta->rowCount() >= 1){
-		$star = ("INSERT INTO avaliacoes (qnt_estrela, user_id, ponto_id, modified) VALUES (?, ?, ?, ?)");
+		$star = ("INSERT INTO avaliacoes (qnt_estrela) VALUES (?)");
 		$queryTwo = $conn->prepare($star);
 		$queryTwo->bindParam(1, $estrela);
-		$queryTwo->bindParam(2, $id);
-		$queryTwo->bindParam(3, $idPonto);
-		$queryTwo->bindParam(4, $date);
-	 	$stmt = $queryTwo->execute();
-		  header('Location: ../view_visualizar_pontos.php?id='.$idPonto);
-	}else{
-		$star = ("INSERT INTO avaliacoes (qnt_estrela, user_id, ponto_id) VALUES (?, ?, ?)");
-		$queryTwo = $conn->prepare($star);
-		$queryTwo->bindParam(1, $estrela);
-		$queryTwo->bindParam(2, $id);
-		$queryTwo->bindParam(3, $idPonto);
 	 	$stmt = $queryTwo->execute();
 		 header('Location: ../view_visualizar_pontos.php?id='.$idPonto);
-	}
 
 	
--- mysql/banco.sql
+++ mysql/banco.sql
@@ -14,6 +14,7 @@ CREATE TABLE `Users` (
 CREATE TABLE `pontos_turisticos` (
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `user_id` int(11) NOT NULL,
+ `aval_id` int(11) NOT NULL,
  `nome_ponto` varchar(200) NOT NULL,
  `logradouro` varchar(200) NOT NULL,
  `bairro` varchar(200) NOT NULL,
@@ -22,18 +23,13 @@ CREATE TABLE `pontos_turisticos` (
  `descricao` varchar(200) NOT NULL,
  `categoria` enum('praia', 'rio', 'praca', 'museu', 'monumento', 'igreja','naturezaparques') NOT NULL,
  PRIMARY KEY (`id`),
- KEY `user_id` (`user_id`),
- CONSTRAINT `pk_users` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-); 
-
+ CONSTRAINT `pk_users` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ CONSTRAINT `pk_aval` FOREIGN KEY (`aval_id`) REFERENCES `avaliacoes` (`id`)
+);
 
 CREATE TABLE `avaliacoes`(
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `user_id` INT(11) NOT NULL,
-    `ponto_id` INT(11) NOT NULL,
     `qnt_estrela` INT(11) NOT NULL,
     `modified` DATETIME DEFAULT NULL,
-    PRIMARY KEY(`id`),
-    CONSTRAINT `pk_av_users` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `pk_av_ponto` FOREIGN KEY(`ponto_id`) REFERENCES `pontos_turisticos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY(`id`)
 );
