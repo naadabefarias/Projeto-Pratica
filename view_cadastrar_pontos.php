fc433b8b90250240009806b58c4a571045d36117
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2018-11-26 15:37:44 -0300

    Implementando função com api onclick para pegar lat e long automaticamente(Não concluído)

--- view_cadastrar_pontos.php
+++ view_cadastrar_pontos.php
@@ -29,7 +29,58 @@ form {
 	font-family: 'Mali', cursive;
 }
 
+
 </style>
+<link href="https://fonts.googleapis.com/css?family=Raleway:500i|Roboto:300,400,700|Roboto+Mono" rel="stylesheet">
+	 <script type="text/javascript">
+        var marker = null;
+        function myMap() {
+            var geocoder = new google.maps.Geocoder;
+            var infowindow = new google.maps.InfoWindow;
+            var myCenter = new google.maps.LatLng(-7.830136823,-34.903713147);
+            var mapCanvas = document.getElementById("map");
+            var mapOptions = {center: myCenter, zoom: 14, mapTypeId: 'roadmap'};
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+
+            map.addListener('click', function(e) {
+                var markerLatLng = e.latLng;
+                document.getElementById("latlng").value = e.latLng.lat() + "," + e.latLng.lng();
+
+                document.getElementById("lat").value = e.latLng.lat()
+                document.getElementById("lng").value = e.latLng.lng();
+
+                geocodeLatLng(geocoder, map, infowindow);
+            });
+        }
+        function geocodeLatLng(geocoder, map, infowindow) {
+            if (marker) {
+                marker.setMap(null);
+            }
+            var input = document.getElementById('latlng').value;
+            var latlngStr = input.split(',', 2);
+            var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+
+            geocoder.geocode({'location': latlng}, function(results, status) {
+                if (status === 'OK') {
+                    if (results[1]) {
+                        marker = new google.maps.Marker({
+                            position: latlng,
+                            map: map
+                        });
+                        infowindow.setContent(results[1].formatted_logradouro);
+                        infowindow.open(map, marker);
+                        document.getElementById("logradouro").value = results[1].formatted_logradouro;
+                    } 
+                    else {
+                        window.alert('No results found');
+                    }
+                } 
+                else {
+                    window.alert('Geocoder failed due to: ' + status);
+                }
+            });
+        }
+    </script>
 <div class="ui container">
 	<h1 id="titulo_cadastro"> Cadastro de Pontos Turísticos </h1>
 	<form method="post" action="Controller/action_cadastrar_ponto.php" class="ui form" enctype="multipart/form-data">
@@ -60,6 +111,8 @@ form {
         <option value="rio">Rio</option>
       </select>
 		<input type="submit" value="Cadastrar" class="ui primary button">
+	
+
 	</form>
 </div>
 
